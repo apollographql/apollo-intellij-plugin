d53ff6a816a05b9d1b9263dd30eee65f173782fa
BoD <BoD@JRAF.org>
2022-12-21 17:59:16 +0100

    Add a confirmation dialog when running migration

--- plugin/src/main/kotlin/com/apollographql/ijplugin/refactoring/migration/ApolloV2ToV3MigrationAction.kt
+++ plugin/src/main/kotlin/com/apollographql/ijplugin/refactoring/migration/ApolloV2ToV3MigrationAction.kt
@@ -1,14 +1,27 @@
 package com.apollographql.ijplugin.refactoring.migration
 
+import com.apollographql.ijplugin.ApolloBundle
 import com.apollographql.ijplugin.util.logd
 import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ui.Messages
 
 class ApolloV2ToV3MigrationAction : AnAction() {
   override fun actionPerformed(e: AnActionEvent) {
     logd()
-    ApolloV2ToV3MigrationProcessor(e.project ?: return).run()
+    val okCancelResult = Messages.showOkCancelDialog(
+      e.project,
+      ApolloBundle.message("action.ApolloV2ToV3MigrationAction.confirmDialog.message"),
+      ApolloBundle.message("action.ApolloV2ToV3MigrationAction.confirmDialog.title"),
+      ApolloBundle.message("action.ApolloV2ToV3MigrationAction.confirmDialog.ok"),
+      ApolloBundle.message("action.ApolloV2ToV3MigrationAction.confirmDialog.cancel"),
+      Messages.getQuestionIcon()
+    )
+
+    if (okCancelResult == Messages.OK) {
+      ApolloV2ToV3MigrationProcessor(e.project ?: return).run()
+    }
   }
 
   override fun update(event: AnActionEvent) {
