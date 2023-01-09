c481ae4228cde3532e6c234103450d5a713bada0
BoD <BoD@JRAF.org>
2023-01-09 11:58:47 +0100

    Fix NPE in runWriteActionInEdt

--- plugin/src/main/kotlin/com/apollographql/ijplugin/util/Threading.kt
+++ plugin/src/main/kotlin/com/apollographql/ijplugin/util/Threading.kt
@@ -2,10 +2,8 @@ package com.apollographql.ijplugin.util
 
 import com.intellij.openapi.application.ApplicationManager
 
-fun <T> runWriteActionInEdt(action: () -> T): T {
-  var result: T? = null
+fun runWriteActionInEdt(action: () -> Unit) {
   ApplicationManager.getApplication().invokeLater {
-    result = ApplicationManager.getApplication().runWriteAction<T>(action)
+    ApplicationManager.getApplication().runWriteAction<Unit>(action)
   }
-  return result!!
 }
