e20ad4e37a27802205002c1f8ea79be9237031c4
BoD <BoD@JRAF.org>
2025-12-01 09:47:45 +0100

    Load all plugins in the root project buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,8 @@
 plugins {
   alias(libs.plugins.kotlin.jvm).apply(false)
   alias(libs.plugins.apollo).apply(false)
-}
-
-buildscript {
-  dependencies {
-    // See https://github.com/JetBrains/intellij-platform-gradle-plugin/issues/2062
-    classpath("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
-  }
+  alias(libs.plugins.intellij.platform).apply(false)
+  alias(libs.plugins.intellij.platform.module).apply(false)
+  alias(libs.plugins.changelog).apply(false)
+  alias(libs.plugins.grammarkit).apply(false)
 }
