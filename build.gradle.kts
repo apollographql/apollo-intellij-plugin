3e90bd2736904a42a84893dd41dd05ebe343a26f
Benoit 'BoD' Lubek <BoD@JRAF.org>
2025-12-01 14:56:54 +0100

    Bump IntelliJ Platform plugin (#100)
    * Bump IntelliJ Platform plugin
    
    * Load all plugins in the root project buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 plugins {
   alias(libs.plugins.kotlin.jvm).apply(false)
   alias(libs.plugins.apollo).apply(false)
+  alias(libs.plugins.intellij.platform).apply(false)
+  alias(libs.plugins.intellij.platform.module).apply(false)
+  alias(libs.plugins.changelog).apply(false)
+  alias(libs.plugins.grammarkit).apply(false)
 }
