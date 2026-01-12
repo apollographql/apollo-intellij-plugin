b0608c954c0e4b8115ad1b5156ab8a149b9a27c3
BoD <BoD@JRAF.org>
2026-01-12 15:31:20 +0100

    Remove temporary logs

--- plugin/src/main/kotlin/com/apollographql/ijplugin/inspection/ApolloUnusedOperationInspection.kt
+++ plugin/src/main/kotlin/com/apollographql/ijplugin/inspection/ApolloUnusedOperationInspection.kt
@@ -6,7 +6,6 @@ import com.apollographql.ijplugin.navigation.findKotlinOperationDefinitions
 import com.apollographql.ijplugin.project.apolloProjectService
 import com.apollographql.ijplugin.telemetry.TelemetryEvent
 import com.apollographql.ijplugin.util.isProcessCanceled
-import com.apollographql.ijplugin.util.logd
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.lang.jsgraphql.psi.GraphQLIdentifier
@@ -19,9 +18,7 @@ class ApolloUnusedOperationInspection : LocalInspectionTool() {
   override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
     return object : GraphQLVisitor() {
       override fun visitTypedOperationDefinition(o: GraphQLTypedOperationDefinition) {
-        logd("XXX Visiting operation definition: ${o.name}")
         if (isUnusedOperation(o)) {
-          logd("XXX Found unused operation: ${o.name}")
           val nameElement = o.childrenOfType<GraphQLIdentifier>().firstOrNull() ?: return
           holder.registerProblem(
               nameElement,
@@ -35,38 +32,21 @@ class ApolloUnusedOperationInspection : LocalInspectionTool() {
 }
 
 fun isUnusedOperation(operationDefinition: GraphQLTypedOperationDefinition): Boolean {
-  logd("XXX Checking if operation is unused: ${operationDefinition.name}")
   if (isProcessCanceled()) return false
-  logd("XXX Apollo version: ${operationDefinition.project.apolloProjectService.apolloVersion}")
   if (!operationDefinition.project.apolloProjectService.apolloVersion.isAtLeastV3) return false
   val ktClasses = findKotlinOperationDefinitions(operationDefinition).ifEmpty {
     // Didn't find any generated class: maybe in the middle of writing a new operation, let's not report an error yet.
-    logd("XXX No generated class found for operation: ${operationDefinition.name}")
     return false
   }
-  logd("XXX Found generated classes for operation ${operationDefinition.name}: ${ktClasses.map { it.name }}")
   val kotlinFindUsagesHandlerFactory = KotlinFindUsagesHandlerFactoryCompat(operationDefinition.project)
   val hasUsageProcessor = HasUsageProcessor()
   for (kotlinDefinition in ktClasses) {
     if (kotlinFindUsagesHandlerFactory.canFindUsages(kotlinDefinition)) {
-      val kotlinFindUsagesHandler = kotlinFindUsagesHandlerFactory.createFindUsagesHandler(kotlinDefinition, false).also {
-        if (it == null) {
-          logd("XXX createFindUsagesHandler returned null for: ${kotlinDefinition.name}")
-        }
-      }
+      val kotlinFindUsagesHandler = kotlinFindUsagesHandlerFactory.createFindUsagesHandler(kotlinDefinition, false)
           ?: return false
-      val findUsageOptions = kotlinFindUsagesHandlerFactory.findClassOptions.also {
-        if (it == null) {
-          logd("XXX findClassOptions returned null for: ${kotlinDefinition.name}")
-        }
-      } ?: return false
-      logd("XXX Calling processElementUsages for ${kotlinDefinition.name}")
+      val findUsageOptions = kotlinFindUsagesHandlerFactory.findClassOptions ?: return false
       kotlinFindUsagesHandler.processElementUsages(kotlinDefinition, hasUsageProcessor, findUsageOptions)
-      logd("XXX Usage found for ${kotlinDefinition.name}: ${hasUsageProcessor.foundUsage}")
       if (hasUsageProcessor.foundUsage) return false
-    } else {
-      logd("XXX canFindUsages returned false for: ${kotlinDefinition.name}")
-      return false
     }
   }
   return true
