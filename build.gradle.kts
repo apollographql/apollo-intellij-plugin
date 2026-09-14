996240f1fa1dfcf05ca2042f66bfcf616f00e3c0
Benoit 'BoD' Lubek <BoD@JRAF.org>
2026-09-14 11:36:53 +0200

    Use org.jetbrains.intellij.platform.settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
   alias(libs.plugins.intellij.platform).apply(false)
   alias(libs.plugins.intellij.platform.module).apply(false)
   alias(libs.plugins.changelog).apply(false)
-  alias(libs.plugins.grammarkit).apply(false)
+  alias(libs.plugins.intellij.platform.grammarkit).apply(false)
 }
